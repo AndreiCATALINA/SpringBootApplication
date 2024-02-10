40e429b8dd2b29ca95fef52681d2810441176b9d
Andrei <catalinaandrei135@gmail.com>
2024-02-10 16:15:57 +0200

    Created tables and connected them. Started the custom CRUD operations

--- src/main/java/com/example/demospringboot/democrudapp/repository/BookRepository.java
+++ src/main/java/com/example/demospringboot/democrudapp/repository/BookRepository.java
@@ -9,9 +9,10 @@ import java.util.List;
 @Repository //reprezinta o interfata care gestioneaza entitatea(book) pentru a stoca si manipula date prin CRUD in DB
 //o clasa extinde o alta clasa cu extends dar o clasa poate sa implementeze mai multe interfete cu implements
 //o interfata poate extinde una sau mai multe interfete cu extends
-public interface BookRepository extends JpaRepository<Book,Long> { //<Entitstea pe care se exewcuta CRUD , tipul cheii primare>
+public interface BookRepository extends JpaRepository<Book, Long> { //<Entitstea pe care se exewcuta CRUD , tipul cheii primare>
     // putem sa ne definim operatii CRUD custom
     //find all books by author
     List<Book> searchBooksByAuthor(String author);
+
     List<Book> searchBooksByNameAndPrice(String name, double price);
 }
